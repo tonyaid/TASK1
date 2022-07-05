3d57641d6d5ee4224aa99fa96e7f4ecec3c06279
Tonya <spectatortonya@gmail.com>
2022-07-05 22:02:34 +0300

    Initial commit

---
+++ src/MAIN.java
@@ -0,0 +1,9 @@
+public class MAIN {
+    public static void main(String[] args) {
+        int price = 6_000;
+        int OneMilePrice = 20;
+
+        int miles = price / OneMilePrice;
+        System.out.println("Бонусные мили: " + miles);
+    }
+}
